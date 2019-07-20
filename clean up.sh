8e142ceee4c952618862bdf7bac353b222e84f64
isaac <IsaackerleY@gmail.com>
2019-07-20 09:55:02 -0600

    Added clean up

---
+++ clean up.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+#rm server.jar
+rm banned-ips.json
+rm banned-players.json
+rm bukkit.yml
+rm commands.yml
+rm eula.txt
+rm help.yml
+rm ops.json
+rm permissions.yml
+rm server.properties
+rm usercache.json
+rm whitelist.json
+rm -r world
+rm -r world_nether
+rm -r world_the_end
